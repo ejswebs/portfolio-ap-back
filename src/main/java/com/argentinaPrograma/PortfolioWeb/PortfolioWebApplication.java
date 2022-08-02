c02e8e1b73d1299872109b4fb477afc0e36e8607
Enzo Joel Steier <johndoe@example.com>
2022-08-02 09:38:58 -0300

    fix: fix de build

--- src/main/java/com/argentinaPrograma/PortfolioWeb/PortfolioWebApplication.java
+++ src/main/java/com/argentinaPrograma/PortfolioWeb/PortfolioWebApplication.java
@@ -1,9 +1,13 @@
 package com.argentinaPrograma.PortfolioWeb;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Getter
+@Setter
 public class PortfolioWebApplication {
 
 	public static void main(String[] args) {
