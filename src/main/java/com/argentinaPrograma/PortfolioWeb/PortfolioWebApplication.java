8fc1f3efb3e1db2f27d539e4a5f99b897116db3d
Enzo Joel Steier <johndoe@example.com>
2022-08-05 14:43:09 -0300

    start class

--- src/main/java/com/argentinaPrograma/PortfolioWeb/PortfolioWebApplication.java
+++ src/main/java/com/argentinaPrograma/PortfolioWeb/PortfolioWebApplication.java
@@ -4,10 +4,14 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @Getter
 @Setter
+@RestController
+@RequestMapping("/api")
 public class PortfolioWebApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/argentinaPrograma/PortfolioWeb/controller/TimeController.java
+++ src/main/java/com/argentinaPrograma/PortfolioWeb/controller/TimeController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api")
+
 public class TimeController {
 
     @GetMapping("/time")
